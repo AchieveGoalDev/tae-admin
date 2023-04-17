c45902f05ee90a9a007b681d259265aeb841b05d
Justin <thejasumasa@gmail.com>
2023-04-17 17:58:03 +0900

    begin formatting for creating a student in modal dialogue

--- src/lib/admin/modals/StudentPersonalModal.svelte
+++ src/lib/admin/modals/StudentPersonalModal.svelte
@@ -16,7 +16,22 @@
   $: console.log($student);
 </script>
 
-<div class="flex flex-col my-4">
+<div class="grid grid-cols-2 my-4　gap-y-0.5">
+  <label class="font-bold self-center justify-self-end" for="学籍番号">
+    学籍番号:
+  </label>
+  <input
+    class="shadow-md ml-2 p-1 self-center justify-self-start"
+    on:input={(e) => handleUpdate("number", e?.target?.value)}
+    name="学籍番号"
+  />
+  <label class="font-bold self-center" for="性別"> 性別: </label>
+  <input
+    class="shadow-md ml-2 p-1 mb-3 self-start"
+    on:input={(e) => handleUpdate("gender", e?.target?.value)}
+    name="性別"
+  />
+
   <label class="font-bold mb-3">
     名前:
     <input
@@ -24,11 +39,11 @@
       class="shadow-md ml-2 p-1"
     />
   </label>
-  <label class="font-bold">
-    性別:
+  <label class="font-bold mb-3">
+    :
     <input
+      on:input={(e) => handleUpdate("name", e?.target?.value)}
       class="shadow-md ml-2 p-1"
-      on:input={(e) => handleUpdate("gender", e?.target?.value)}
     />
   </label>
   <div>
---
+++ src/lib/admin/modals/inputs/TextInput.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  export let label: string;
+  export let value: string;
+</script>
+
+<label class="font-bold self-center justify-self-end" for="学籍番号">
+  学籍番号:
+</label>
+<input
+  class="shadow-md ml-2 p-1 self-center justify-self-start"
+  on:input={(e) => handleUpdate("number", e?.target?.value)}
+  name="学籍番号"
+/>
