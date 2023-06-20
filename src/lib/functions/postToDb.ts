445198755c8d8dd57a29d0f01b90c5ca6c072e92
Jasumasa <thejasumasa@gmail.com>
2023-06-20 20:31:31 +0900

    create utility function for making api calls, create lambda function for event api, center headings on admin table

---
+++ src/lib/functions/postToDb.ts
@@ -0,0 +1,21 @@
+export async function handleDBPut(data: any, url: string) {
+    try {
+        const response = await (fetch(url), {
+            mode: "cors",
+            method: "post",
+            cache: "no-cache",
+            credentials: "include",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            referrerPolicy: "no-referrer",
+            body: JSON.stringify(data)
+        })
+        console.log("API call success")
+        console.log(JSON.stringify(response))
+    } catch (err) {
+        console.log("API call error")
+        console.log(err)
+    }
+
+}
